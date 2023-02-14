9e7148d2b37a83e09d5e1cd66a370d2f7e9c530e
GuilhermeMirandaCarvalho <guilhermeinfo1@gmail.com>
2023-02-14 09:47:10 -0300

    Criando Controlador Produtos e Categorias

--- APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -29,6 +29,66 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
+        [HttpGet("{id:int}", Name="Obter Produto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p=> p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado");
+
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("Obter Produto", new { id = produto.ProdutoId }, produto);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto) 
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p=> p.ProdutoId == id);
+            //var produto = _context.Produtos.Find(id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+
     }
     
 
